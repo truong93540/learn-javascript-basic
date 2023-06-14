ad379da7e9b05b09619e6b78c06f6146bad6e113
Nguyen Van Truong <truong93540@gmail.com>
2023-06-15 00:30:38 +0700

    20230615

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ var courses = [
     {
         id: 1,
         name: 'Javascript',
-        coin: 250
+        coin: 0
     },
     {
         id: 2,
         name: 'HTML, CSS',
-        coin: 0
+        coin: 568
     },
     {
         id: 3,
@@ -28,12 +28,12 @@ var courses = [
     {
         id: 4,
         name: 'PHP',
-        coin: 400
+        coin: 723
     },
     {
         id: 5,
         name: 'Ruby',
-        coin: 500
+        coin: 498
     }
 ];
 
@@ -70,19 +70,63 @@ var courses = [
 
 // Tự tạo ra phương thức some
 
-Array.prototype.some2 = function(callback) {
-    for(var index in this){
-        if(this.hasOwnProperty(index)){// kiểm tra key liền kề
-            if(callback(this[index], index)){
-                return true
-            }
-        }    
-    }
-    return false
-}
+// Array.prototype.some2 = function(callback) {
+//     for(var index in this){
+//         if(this.hasOwnProperty(index)){// kiểm tra key liền kề
+//             if(callback(this[index], index)){
+//                 return true
+//             }
+//         }    
+//     }
+//     return false
+// }
 
-var isFree = courses.some2(function(course, index) {
-    return course.coin === 0;
-})
+// var isFree = courses.some2(function(course, index) {
+//     return course.coin === 0;
+// })
 
-console.log(isFree)
+// console.log(isFree)
+
+// Tự tạo ra phương thức every
+
+// Array.prototype.every2 = function(callback) {
+//     var result = true;
+//     for(var index in this){
+//         if(this.hasOwnProperty(index) && !callback(this[index], index, this)){
+//             result = false;
+//             break;
+//         }
+//     }
+//     return result;
+// }
+
+// var isFree = courses.every2(function(course, index, arr) {
+//     return course.coin === 0;
+// })
+
+// console.log(isFree)
+
+
+
+// HTML DOM
+
+// Có 3 thành phần
+//1. Element
+//2. Attribute
+//3. Text
+
+
+// Node
+
+// Javascript(cung cấp bộ phương tiện truy xuất vào HTML DOM): Brower | Server (NodeJS)
+
+// Application programing interface
+
+// Browser: HTML -> DOM -> WEB API (chỉ tồn tại ở trên môi trường web)
+
+
+
+// Document object(đại diện cho toàn bộ website)
+// Mỗi khi muốn truy xuất vào các thành phần nhỏ thì phải đi qua document
+
+document.write('HELLO GUYS!') // thêm chuỗi vào vị trí mà mình link
